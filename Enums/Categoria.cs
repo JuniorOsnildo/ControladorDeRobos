4bb1a0ca55b287e94e54398728af08cedb3e3464
isadorabsagaz <isadora.sagaz@gmail.com>
2025-04-23 15:22:05 -0300

    feat: mapa do deposito

---
+++ Enums/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace ControladorDeRobos.Enums;
+
+public enum Categoria
+{
+    LocalEntrega,   //"X"
+    Robo,           //"R."
+    Bloqueado,      //"-1"
+    Estante,        //"numeros"
+    Livre           //""
+}
---
+++ Services/MapaService.cs
@@ -0,0 +1,62 @@
+﻿using ControladorDeRobos.Enums;
+using ControladorDeRobos.Models;
+
+namespace ControladorDeRobos.Services;
+
+public class MapaService
+{
+    public static Celula[,] GerarMapa()
+    {
+        var mapa = new Celula[13, 15];
+        
+        string[,] dadosMapa = new string[13, 15]
+        {
+            {"", "", "", "", "", "", "", "", "", "", "", "", "", "", ""},
+            {"1", "", "11", "21", "", "31", "41", "", "51", "61", "", "71", "81", "", "91"},
+            {"2", "", "12", "22", "", "32", "42", "", "52", "62", "", "72", "82", "", "92"},
+            {"3", "", "13", "23", "", "33", "43", "", "53", "63", "", "73", "83", "", "93"},
+            {"4", "", "14", "24", "", "34", "44", "", "54", "64", "", "74", "84", "", "94"},
+            {"5", "", "15", "25", "", "35", "45", "", "55", "65", "", "75", "85", "", "95"},
+            {"6", "", "16", "26", "", "36", "46", "", "56", "66", "", "76", "86", "", "96"},
+            {"7", "", "17", "27", "", "37", "47", "", "57", "67", "", "77", "87", "", "97"},
+            {"8", "", "18", "28", "", "38", "48", "", "58", "68", "", "78", "88", "", "98"},
+            {"9", "", "19", "29", "", "39", "49", "", "59", "69", "", "79", "89", "", "99"},
+            {"10","", "20", "30", "", "40", "50", "", "60", "70", "", "80", "90", "", "100"},
+            {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "X"},
+            {"R1", "R2", "R3", "R4", "R5", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1"}
+        };
+
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                string valor = dadosMapa[i, j];
+
+                if (valor != "")
+                {
+                    if (valor == "X")
+                    {
+                        mapa[i, j] = new Celula(Categoria.LocalEntrega, valor);
+                    }
+                    else if (valor.StartsWith("R"))
+                    {
+                        mapa[i, j] = new Celula(Categoria.Robo, valor, false);
+                    }
+                    else if (valor == "-1")
+                    {
+                        mapa[i, j] = new Celula(Categoria.Bloqueado, valor, false);
+                    }
+                    else
+                    {
+                        mapa[i, j] = new Celula(Categoria.Estante, valor);
+                    }
+                    
+                }
+                else
+                    mapa[i, j] = new Celula(Categoria.Livre, valor);
+            }
+        }
+        return mapa;
+    }
+    
+}
