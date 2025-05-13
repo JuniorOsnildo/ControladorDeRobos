ba8d9bb23040fe16bc4707d7e7a2ee4f2195bde2
Junior Osnildo <juniorosnildo777@gmail.com>
2025-05-13 11:50:58 -0300

    aaaaaaaaa

--- Controllers/RotaController.cs
+++ Controllers/RotaController.cs
@@ -1,4 +1,5 @@
-﻿using ControladorDeRobos.Models;
+﻿using ControladorDeRobos.DTOs;
+using ControladorDeRobos.Models;
 using ControladorDeRobos.Services;
 using ControladorDeRobos.Services.Buscas;
 using Microsoft.AspNetCore.Mvc;
@@ -9,16 +10,18 @@ namespace ControladorDeRobos.Controllers;
 [Route("[controller]")]
 public class RotaController(IConfiguration configuracao) : ControllerBase
 {
-    [HttpGet]
-    [Route("/busca/{xEstante:int}/{yEstante:int}/{tipoBusca}")]
-    public IActionResult EncontrarRoboECaminho(int xEstante, int yEstante, string tipoBusca)
+    [HttpPost]
+    [Route("/busca")]
+    public IActionResult EncontrarRoboECaminho([FromBody] AlgoritimoDto algoritimoDto)
     {
-        var algoritmo = BuscaFactory.GetAlgoritmo(tipoBusca);
-        var (melhorRobo, melhorCaminho) = BuscaService.EncontrarRoboMaisProximo(algoritmo, xEstante, yEstante);
+        
+        
+        var algoritmo = BuscaFactory.GetAlgoritmo(algoritimoDto.TipoBusca);
+        var (melhorRobo, melhorCaminho) = BuscaService.EncontrarRoboMaisProximo(algoritmo, algoritimoDto.X, algoritimoDto.Y);
         
         if (melhorCaminho == null) return NotFound();
         
-        var rota = RotaService.GerarRotaCompleta(melhorCaminho, xEstante, yEstante);
+        var rota = RotaService.GerarRotaCompleta(melhorCaminho, algoritimoDto.X, algoritimoDto.Y);
         return Ok(new {melhorRobo, rota});
     }
 }
---
+++ DTOs/AlgoritimoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ControladorDeRobos.DTOs;
+
+public class AlgoritimoDto
+{
+    public int X { get; set; } 
+    public int Y { get; set; }
+    public string TipoBusca { get; set; }
+}
